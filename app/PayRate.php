492e08f7c6a2f52023e2e7a20617d066b77ba59a
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-16 20:29:54 +0100

    Pay rates are now saved in cents.

--- app/PayRate.php
+++ app/PayRate.php
@@ -10,6 +10,30 @@ class PayRate extends Model
 
     protected $table = 'pay_rates';
 
+    /**
+     * The rate is stored in cents, so it needs to be devided by 100
+     * when retrieved from the database.
+     *
+     * @param Integer $rate The raw hourly pay rate from the database.
+     *
+     * @return Integer|Float The hourly pay rate in euros.
+     */
+    public function getRateAttribute($rate)
+    {
+        return number_format($rate / 100, 2);
+    }
+
+    /**
+     * The rate is stored in cents, so it needs to be multiplied
+     * by 100 before storing it into the database.
+     *
+     * @param Integer|Float $rate The hourly pay rate in euros.
+     */
+    public function setRateAttribute($rate)
+    {
+        $this->attributes['rate'] = $rate * 100;
+    }
+
     public function invoiceDetails()
     {
         return $this->belongsToMany(InvoiceDetails::class);
