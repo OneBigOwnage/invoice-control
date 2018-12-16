efc4bb6561d17b6092849a483382edc469aa4bb7
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-16 20:30:03 +0100

    Remove useless attributes from HTML.

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -86,7 +86,7 @@ Customers
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Confirm delete customer</h5>
+        <h5 class="modal-title">Confirm delete customer</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
--- resources/views/payrates/index.blade.php
+++ resources/views/payrates/index.blade.php
@@ -92,7 +92,7 @@ Pay rates
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Confirm delete pay rate</h5>
+        <h5 class="modal-title">Confirm delete pay rate</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
