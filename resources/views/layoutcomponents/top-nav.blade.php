3e96d51aefd7e9dc9abaa40380cd9fa196524a3b
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-19 21:54:19 +0100

    Fix search box height.

--- resources/views/layoutcomponents/top-nav.blade.php
+++ resources/views/layoutcomponents/top-nav.blade.php
@@ -53,10 +53,12 @@
                 <!-- ============================================================== -->
                 <!-- Search -->
                 <!-- ============================================================== -->
-                <li class="nav-item search-box"> <a class="nav-link waves-effect waves-dark" href="javascript:void(0)"><i
-                            class="ti-search"></i></a>
+                <li class="nav-item search-box">
+                    <a class="nav-link waves-effect waves-dark" href="javascript:void(0)">
+                        <i class="ti-search"></i>
+                    </a>
                     <form class="app-search position-absolute">
-                        <input type="text" class="form-control" placeholder="Search &amp; enter">
+                        <input type="text" class="form-control" style="height: unset;" placeholder="Search &amp; enter">
                         <a class="srh-btn">
                             <i class="ti-close"></i>
                         </a>
