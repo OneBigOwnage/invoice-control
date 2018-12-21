3c9e8b601333b6cade0a3f57f9c5735943463e36
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-21 22:32:17 +0100

    Add invoicedetails and phpdoc to factory.

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,20 +2,29 @@
 
 use App\Customer;
 use App\Invoice;
+use App\InvoiceDetails;
+use Illuminate\Database\Eloquent\Factory;
 use Faker\Generator as Faker;
+
+/**
+ * @var Factory $factory
+ */
+$factory;
 
 $factory->define(Invoice::class, function (Faker $faker) {
     return [
         'customer_id' => function($invoiceData) {
             return factory(Customer::class)->create()->id;
         },
-        'is_completed' => !rand(0, 1),
-        'sub_total' => function($invoiceData) use ($faker) {
-            if (rand(0, 1)) {
-                return $faker->numberBetween(0, 999);
-            }
-
-            return null;
-        }
+        'is_completed' => false,
+        'sub_total' => null
     ];
+});
+
+$factory->afterCreatingState(Invoice::class, 'completed', function (Invoice $invoice, Faker $faker) {
+    $details = factory(InvoiceDetails::class, $faker->numberBetween(1, 9))->make();
+
+    $invoice->details()->saveMany($details);
+
+    $invoice->is_completed = true;
 });
