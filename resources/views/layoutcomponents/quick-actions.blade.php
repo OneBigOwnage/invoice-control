cf1ec6e220def4b810bbd91cc61c23a8d3ef1a8b
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-22 13:36:03 +0100

    Add quick-actions.

---
+++ resources/views/layoutcomponents/quick-actions.blade.php
@@ -0,0 +1,26 @@
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" id="2" data-toggle="dropdown" aria-haspopup="true"
+    aria-expanded="false"> <i class="font-24 mdi mdi-chart-bubble"></i>
+  </a>
+  <div class="dropdown-menu dropdown-menu-right mailbox animated bounceInDown" aria-labelledby="2">
+    <ul class="list-style-none">
+      <li>
+
+
+        <a href="/invoices/create" class="link border-top">
+          <div class="d-flex no-block align-items-center p-10">
+            <span class="btn btn-success btn-circle">
+              <span class="mdi mdi-file-document"></span>
+            </span>
+            <div class="m-l-10">
+              <h5 class="m-b-0">Create new invoice</h5>
+              <span class="mail-desc">Quickly create a new invoice.</span>
+            </div>
+          </div>
+        </a>
+
+
+      </li>
+    </ul>
+  </div>
+</li>
