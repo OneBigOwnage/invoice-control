010c1e22ee42a979f54cd7b4f111ebfcd73e7e82
OneBigOwnage <niekvandenbos@hotmail.com>
2018-12-24 10:34:22 +0100

    Invoice entity now makes use of view composers.

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\ViewComposers\PayRates\PayRateCreateComposer;
 use App\ViewComposers\PayRates\PayRateEditComposer;
+use App\ViewComposers\Invoices\InvoiceCreateComposer;
+
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,6 +20,8 @@ class ViewComposerServiceProvider extends ServiceProvider
     {
         View::composer('payrates.create', PayRateCreateComposer::class);
         View::composer('payrates.edit', PayRateEditComposer::class);
+
+        View::composer('invoices.create', InvoiceCreateComposer::class);
     }
 
     /**
